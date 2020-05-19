dcf3dbb8e6ea89343901243fdde0cb2d5c3ee44f
Jonathan Wee <jonathanwee93@gmail.com>
2020-05-19 17:10:19 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, Response, send_file
+import cv2
+
+#from display_depth_frame import rgb_frame
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def gen():
+    while True:
+        #filename = r'C:\Users\User\Desktop\New folder\Savedimg.jpg'
+        img = cv2.imread("Savedimg.jpg")
+        img = cv2.resize(img, (0,0), fx=0.5, fy=0.5) 
+        frame = cv2.imencode('.jpg', img)[1].tobytes()
+        yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+   
+   # filename = r'C:\Users\User\Desktop\New folder\Savedimg.jpg'
+    #return send_file(filename, mimetype='image/jpg')
+    #Response(gen(rgb_frame()),
+    #                mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000', debug=True)
+
